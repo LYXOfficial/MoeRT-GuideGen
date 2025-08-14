2ca2c741766946754667be5b6193e9e53f2be8e0
LYXOfficial <lyx2010official@outlook.com>
2025-08-14 13:52:22 +0800

    新建文件夹！

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
-​
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),WindiCSS(),],
